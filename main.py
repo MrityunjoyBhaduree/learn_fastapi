19ce00aea42d0f62feedd9765e964e889365247b
Mrityunjoy Bhaduree <mrityunjoy.zicore@gmail.com>
2025-11-01 12:38:56 +0600

    add sort api into main file

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import json
 
-from fastapi import FastAPI, Path, HTTPException
+from fastapi import FastAPI, Path, HTTPException, Query
 
 
 app = FastAPI()
@@ -32,3 +32,20 @@ def patient_details(patient_id: str = Path(..., description="Id of the patient i
     if patient_id in data:
         return data[patient_id]
     raise HTTPException(status_code=404, detail="Patient Not Found")
+
+@app.get("/sort")
+def sort_patients(sort_by: str = Query(
+    ..., description="Sort on the basis of height, weight or bmi"),
+    order: str = Query("", description="Sort in asc or desc order")):
+    valid_fields = ["height", "weight", "bmi"]
+    if sort_by not in valid_fields:
+        raise HTTPException(status_code=400, detail=f"Invalid field select from{valid_fields}")
+    if order not in ["asc", "desc"]:
+        raise HTTPException(status_code=400, detail="Invalid order select between 'asc', 'desc'")
+    data = load_data()
+
+    sort_order = True if order == "desc" else False
+
+    sorted_order = sorted(data.values(), key=lambda x:x.get(sort_by, 0),
+                          reverse=sort_order)
+    return sorted_order
